96960d0fedb643158a214cfd2df6b24db5debfdb
CovidHunter <covidhunter036@gmail.com>
2017-12-31 07:35:12 +0900

    working on previous year indicator

--- src/components/ProjectTable/index.js
+++ src/components/ProjectTable/index.js
@@ -1,20 +1,20 @@
-import React from 'react'
+import React from 'react';
 
-import Description from './Description'
-import ProjectAvatar from '../ProjectAvatar'
-import Stars from '../Stars'
+import Description from './Description';
+import ProjectAvatar from '../ProjectAvatar';
+import Stars from '../Stars';
 
-const ProjectTableView = ({
-  title,
-  comment,
-  icon,
-  projects,
-  showStars,
-  showDelta = true,
-  deltaFilter = 'yearly'
-}) => {
-  
-  const maxDelta = projects.map(p => p.delta).reduce((a, b) => Math.max(a,b))
+const trends = [
+  { name: 'downfast', value: -70 },
+  { name: 'down', value: -30 },
+  { name: 'downslow', value: 0 },
+  { name: 'upslow', value: 30 },
+  { name: 'up', value: 70 },
+  { name: 'upfast', value: 9999 },
+];
+
+const ProjectTableView = ({ title, comment, icon, projects, showStars, showDelta = true, deltaFilter = 'yearly' }) => {
+  const maxDelta = projects.map(p => p.delta).reduce((a, b) => Math.max(a, b));
 
   return (
     <div className="project-table">
@@ -27,11 +27,12 @@ const ProjectTableView = ({
           showDelta={showDelta}
           deltaFilter={deltaFilter}
           index={i + 1}
+          trendPercent={Math.round(Math.random() * 200) - 100}
         />
       ))}
     </div>
-  )
-}
+  );
+};
 
 ProjectTableView.Row = ({
   project,
@@ -41,35 +42,47 @@ ProjectTableView.Row = ({
   showDescription = true,
   index,
   maxDelta,
+  trendPercent,
 }) => {
-  const url = project.url || project.repository
+  const url = project.url || project.repository;
 
   // use relative scale
-  const widthPercent = project.delta*100/maxDelta
+  const widthPercent = project.delta * 100 / maxDelta
+
+  const previousWidthPercent = widthPercent + widthPercent*trendPercent/100
 
   // use absolute scale based on Vue as the max
   // const widthPercent = project.delta*100/39263
-  
+
+  const trend = trends.find(trend => {
+    return trendPercent < trend.value;
+  });
+  const trendClass = trend ? trend.name : 'na';
+
   return (
-    <a className="project-table-row" href={url}>
-      <div className="project-table-bar" style={{width: `${widthPercent}%`}}/>
-      <ProjectAvatar project={project} size={50} />
-      <div className="main-column">
-        <div className="row-1">
-          <div>
-            <span className="project-table-rank">#{index}</span>
-            <span>{project.name}</span>
-          </div>
-          <div>
+    <a className={`project-table-row project-table-row-${trendClass}`} href={url}>
+      <div className="project-table-bar" style={{ width: `${widthPercent}%` }} />
+      <div className="project-table-bar project-table-bar-previous" style={{ width: `${previousWidthPercent}%` }} />
+      <div className="project-table-contents">
+        <ProjectAvatar project={project} size={50} />
+        <div className="main-column">
+          <div className="row-1">
+            <div>
+              <span className="project-table-rank">#{index}</span>
+              <span>{project.name}</span>
+            </div>
+            <div>
             <Stars value={project.delta} decimals={1} />
+            <span className="project-trend">{trendPercent > 0 ? `+${trendPercent}` : trendPercent}%</span>
+            </div>
           </div>
-        </div>
-        <div className="description-section">
-          <Description text={project.description} />
+          <div className="description-section">
+            <Description text={project.description} />
+          </div>
         </div>
       </div>
     </a>
-  )
-}
+  );
+};
 
-export default ProjectTableView
+export default ProjectTableView;
